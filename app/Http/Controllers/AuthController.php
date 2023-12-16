7bab3bb5afb385dab234a2846d3221ca11b7328b
Teguh Susanto <teguh.susanto@hotmail.com>
2023-12-16 09:23:08 +0700

    handle database error

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,7 @@ use Auth;
 use DateInterval;
 use Laravel\Sanctum\PersonalAccessToken;
 use App\Http\Controllers\BaseController;
+
 const ERR_INVALID_REFRESH_TOKEN = 'ERR_INVALID_REFRESH_TOKEN';
 const ERR_INVALID_CREDS = 'ERR_INVALID_CREDS';
 class AuthController extends BaseController
@@ -19,61 +20,68 @@ class AuthController extends BaseController
             'email' => 'required|email',
             'password' => 'required',
         ]);
-        
+
         if (!auth()->attempt($credentials)) {
-            return $this->sendError(ERR_INVALID_CREDS,"incorrect username or password",401); 
+            return $this->sendError(ERR_INVALID_CREDS, "incorrect username or password", 401);
         }
-        
-        $user = auth()->user();
-        $date = new DateTimeImmutable();
-        $expiresAt = $date->add(new DateInterval('PT'.config('sanctum.expiration').'M'));
-        $accessToken = $user->createToken('access_token', [TokenAbility::ACCESS_API->value],$expiresAt );
-        $date = new DateTimeImmutable();
-        $RTexpiresAt = $date->add(new DateInterval('PT'.config('sanctum.rt_expiration').'M'));
-        $accessToken = $user->createToken('access_token', [TokenAbility::ACCESS_API->value],$expiresAt );
-        $refreshToken = $user->createToken('refresh_token', [TokenAbility::ISSUE_ACCESS_TOKEN->value], $RTexpiresAt);
-        return response()->json([
-            'ok' => true,
-            'data' => [
-                'user' => [
-                    'id' => $user->id,
-                    'email' => $user->email,
-                    'name' => $user->name,
+        try {
+            $user = auth()->user();
+            $date = new DateTimeImmutable();
+            $expiresAt = $date->add(new DateInterval('PT' . config('sanctum.expiration') . 'M'));
+            $accessToken = $user->createToken('access_token', [TokenAbility::ACCESS_API->value], $expiresAt);
+            $date = new DateTimeImmutable();
+            $RTexpiresAt = $date->add(new DateInterval('PT' . config('sanctum.rt_expiration') . 'M'));
+            $accessToken = $user->createToken('access_token', [TokenAbility::ACCESS_API->value], $expiresAt);
+            $refreshToken = $user->createToken('refresh_token', [TokenAbility::ISSUE_ACCESS_TOKEN->value], $RTexpiresAt);
+            return response()->json([
+                'ok' => true,
+                'data' => [
+                    'user' => [
+                        'id' => $user->id,
+                        'email' => $user->email,
+                        'name' => $user->name,
+                    ],
+                    'token' => $accessToken->plainTextToken,
+                    'refresh_token' => $refreshToken->plainTextToken,
                 ],
-                'token' => $accessToken->plainTextToken,
-                'refresh_token' => $refreshToken->plainTextToken,
-            ],
-        ]);
+            ]);
+        } catch (\Illuminate\Database\QueryException $exception) {
+            return $this->sendError(ERR_INTERNAL_ERROR, "unable to connect into database", 500);
+        }
     }
     public function refresh(Request $request)
     {
         if (!empty($request->header('Authorization'))) {
             $parts = explode(' ', $request->header('Authorization'));
-    
+
             if (count($parts) !== 2 || strtolower($parts[0]) !== 'bearer') {
-                return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+                return $this->sendError(ERR_INVALID_REFRESH_TOKEN, "invalid refresh token", 401);
             }
             $refreshToken = $parts[1];
         } else {
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+            return $this->sendError(ERR_INVALID_REFRESH_TOKEN, "invalid refresh token", 401);
         }
-        $token = PersonalAccessToken::findToken($refreshToken);
-        if(empty($token)){
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
-        } else {
-            $user =  \App\Models\User::find($token->tokenable_id);
-            $date = new DateTimeImmutable();
-            $newDate = $date->add(new DateInterval('PT'.config('sanctum.expiration').'M'));
-            $accessToken = $user->createToken('access_token', [TokenAbility::ACCESS_API->value],$newDate );
-            
-            return response()->json([
-                'ok' => true,
-                'data' => [
-                    'access_token' => $accessToken->plainTextToken
-                ],
-            ]);
+        try {
+            $token = PersonalAccessToken::findToken($refreshToken);
+            if (empty($token)) {
+                return $this->sendError(ERR_INVALID_REFRESH_TOKEN, "invalid refresh token", 401);
+            } else {
+                $user = \App\Models\User::find($token->tokenable_id);
+                $date = new DateTimeImmutable();
+                $newDate = $date->add(new DateInterval('PT' . config('sanctum.expiration') . 'M'));
+                $accessToken = $user->createToken('access_token', [TokenAbility::ACCESS_API->value], $newDate);
+
+                return response()->json([
+                    'ok' => true,
+                    'data' => [
+                        'access_token' => $accessToken->plainTextToken
+                    ],
+                ]);
+            }
+        } catch (\Illuminate\Database\QueryException $exception) {
+            return $this->sendError(ERR_INTERNAL_ERROR, "unable to connect into database", 500);
         }
-        
+
     }
     public function logout()
     {
--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -9,10 +9,12 @@ use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\BaseController as BaseController;
 use App\Http\Resources\ReminderResource;
 use Validator;
-const ERR_INVALID_REFRESH_TOKEN = 'ERR_INVALID_REFRESH_TOKEN';
-const ERR_INVALID_CREDS = 'ERR_INVALID_CREDS';
+
+const ERR_INVALID_ACCESS_TOKEN = 'ERR_INVALID_ACCESS_TOKEN';
+const ERR_FORBIDDEN_ACCESS = 'ERR_FORBIDDEN_ACCESS';
 const ERR_REQUIRED = 'ERR_REQUIRED';
 const ERR_NOT_FOUND = 'ERR_NOT_FOUND';
+const ERR_INTERNAL_ERROR = "ERR_INTERNAL_ERROR";
 class ReminderController extends BaseController
 {
     /**
@@ -22,23 +24,29 @@ class ReminderController extends BaseController
     {
         if (!empty($request->header('Authorization'))) {
             $parts = explode(' ', $request->header('Authorization'));
-    
+
             if (count($parts) !== 2 || strtolower($parts[0]) !== 'bearer') {
-                return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+                return $this->sendError(ERR_INVALID_ACCESS_TOKEN, "invalid access token", 401);
             }
             $refreshToken = $parts[1];
         } else {
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+            return $this->sendError(ERR_INVALID_ACCESS_TOKEN, "invalid access token", 401);
         }
-        $token = PersonalAccessToken::findToken($refreshToken);
-        if(empty($token)){
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
-        } else {
-            $limit = $request->query('limit', 10); // Get limit from query string
-            $reminder = Reminder::query()->limit($limit)->get();
-            return $this->sendResponse(['reminders'=>ReminderResource::collection($reminder)], $limit,200);
+        try {
+            $token = PersonalAccessToken::findToken($refreshToken);
+            if (empty($token)) {
+                return $this->sendError(ERR_FORBIDDEN_ACCESS, "user doesn't have enough authorization", 403);
+            } else {
+
+                $limit = $request->query('limit', 10); // Get limit from query string
+                $reminder = Reminder::query()->limit($limit)->get();
+                return $this->sendResponse(['reminders' => ReminderResource::collection($reminder)], $limit, 200);
+
+            }
+        } catch (\Illuminate\Database\QueryException $exception) {
+            return $this->sendError(ERR_INTERNAL_ERROR, "unable to connect into database", 500);
         }
-        
+
     }
 
     /**
@@ -56,66 +64,74 @@ class ReminderController extends BaseController
     {
         if (!empty($request->header('Authorization'))) {
             $parts = explode(' ', $request->header('Authorization'));
-    
+
             if (count($parts) !== 2 || strtolower($parts[0]) !== 'bearer') {
-                return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+                return $this->sendError(ERR_INVALID_ACCESS_TOKEN, "invalid access token", 401);
             }
             $refreshToken = $parts[1];
         } else {
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+            return $this->sendError(ERR_INVALID_ACCESS_TOKEN, "invalid access token", 401);
         }
-        $token = PersonalAccessToken::findToken($refreshToken);
-        if(empty($token)){
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
-        } else {
-            $validator = Validator::make($request->json()->all(), [
-                'title' => 'required',
-                'description' => 'required',
-                'remind_at' => 'required|integer',
-                'event_at' => 'required|integer',
-            ]);
-    
-            if($validator->fails()){
-                return $this->sendError(ERR_REQUIRED, $validator->errors(),401);       
+        try {
+            $token = PersonalAccessToken::findToken($refreshToken);
+            if (empty($token)) {
+                return $this->sendError(ERR_FORBIDDEN_ACCESS, "user doesn't have enough authorization", 403);
+            } else {
+                $validator = Validator::make($request->json()->all(), [
+                    'title' => 'required',
+                    'description' => 'required',
+                    'remind_at' => 'required|integer',
+                    'event_at' => 'required|integer',
+                ]);
+
+                if ($validator->fails()) {
+                    return $this->sendError(ERR_REQUIRED, $validator->errors(), 401);
+                }
+
+                $reminder = Reminder::create([
+                    'title' => $request->json('title'),
+                    'description' => $request->json('description'),
+                    'remind_at' => $request->json('remind_at'),
+                    'event_at' => $request->json('event_at'),
+                ]);
+
+                return $this->sendResponse(new ReminderResource($reminder), 0, 201);
             }
-    
-            $reminder = Reminder::create([
-                'title'=>$request->json('title'),
-                'description'=>$request->json('description'),
-                'remind_at'=>$request->json('remind_at'),
-                'event_at'=>$request->json('event_at'),
-            ]);
-    
-            return $this->sendResponse(new ReminderResource($reminder), 0,201);
+        } catch (\Illuminate\Database\QueryException $exception) {
+            return $this->sendError(ERR_INTERNAL_ERROR, "unable to connect into database", 500);
         }
-    } 
+    }
 
     /**
      * Display the specified resource.
      */
-    public function show(Request $request,$id) : JsonResponse
+    public function show(Request $request, $id): JsonResponse
     {
         if (!empty($request->header('Authorization'))) {
             $parts = explode(' ', $request->header('Authorization'));
-    
+
             if (count($parts) !== 2 || strtolower($parts[0]) !== 'bearer') {
-                return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+                return $this->sendError(ERR_INVALID_ACCESS_TOKEN, "invalid access token", 401);
             }
             $refreshToken = $parts[1];
         } else {
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+            return $this->sendError(ERR_INVALID_ACCESS_TOKEN, "invalid access token", 401);
         }
-        $token = PersonalAccessToken::findToken($refreshToken);
-        if(empty($token)){
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
-        } else {
-            $reminder = Reminder::where('id',$id)->get();
-            $reminder = Reminder::find($id);
-            if($reminder==null){
-                return $this->sendError(ERR_NOT_FOUND, 'data has not found',404);  
-            }else{
-                return $this->sendResponse(ReminderResource::collection($reminder), 0,200);
+        try {
+            $token = PersonalAccessToken::findToken($refreshToken);
+            if (empty($token)) {
+                return $this->sendError(ERR_FORBIDDEN_ACCESS, "user doesn't have enough authorization", 403);
+            } else {
+                $reminder = Reminder::where('id', $id)->get();
+                $reminder = Reminder::find($id);
+                if ($reminder == null) {
+                    return $this->sendError(ERR_NOT_FOUND, 'resource is not found', 404);
+                } else {
+                    return $this->sendResponse(ReminderResource::collection($reminder), 0, 200);
+                }
             }
+        } catch (\Illuminate\Database\QueryException $exception) {
+            return $this->sendError(ERR_INTERNAL_ERROR, "unable to connect into database", 500);
         }
     }
 
@@ -134,41 +150,45 @@ class ReminderController extends BaseController
     {
         if (!empty($request->header('Authorization'))) {
             $parts = explode(' ', $request->header('Authorization'));
-    
+
             if (count($parts) !== 2 || strtolower($parts[0]) !== 'bearer') {
-                return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+                return $this->sendError(ERR_INVALID_ACCESS_TOKEN, "invalid access token", 401);
             }
             $refreshToken = $parts[1];
         } else {
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+            return $this->sendError(ERR_INVALID_ACCESS_TOKEN, "invalid access token", 401);
         }
-        $token = PersonalAccessToken::findToken($refreshToken);
-        if(empty($token)){
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
-        } else {
-            $validator = Validator::make($request->json()->all(), [
-                'title' => 'required',
-                'description' => 'required',
-                'remind_at' => 'required|integer',
-                'event_at' => 'required|integer',
-            ]);
-    
-            if($validator->fails()){
-                return $this->sendError(ERR_REQUIRED, $validator->errors(),401);       
+        try {
+            $token = PersonalAccessToken::findToken($refreshToken);
+            if (empty($token)) {
+                return $this->sendError(ERR_FORBIDDEN_ACCESS, "user doesn't have enough authorization", 403);
+            } else {
+                $validator = Validator::make($request->json()->all(), [
+                    'title' => 'required',
+                    'description' => 'required',
+                    'remind_at' => 'required|integer',
+                    'event_at' => 'required|integer',
+                ]);
+
+                if ($validator->fails()) {
+                    return $this->sendError(ERR_REQUIRED, $validator->errors(), 401);
+                }
+
+                $reminder = Reminder::find($id);
+                if ($reminder == null) {
+                    return $this->sendError(ERR_NOT_FOUND, 'resource is not found', 404);
+                } else {
+                    $reminder->title = $request->json('title');
+                    $reminder->description = $request->json('description');
+                    $reminder->remind_at = $request->json('remind_at');
+                    $reminder->event_at = $request->json('event_at');
+                    $reminder->save();
+                    return $this->sendResponse(new ReminderResource($reminder), 0, 201);
+                }
+
             }
-            
-            $reminder = Reminder::find($id);
-            if($reminder==null){
-                return $this->sendError(ERR_NOT_FOUND, 'data has not found',404);  
-            }else{
-                $reminder->title = $request->json('title');
-                $reminder->description=$request->json('description');
-                $reminder->remind_at=$request->json('remind_at');
-                $reminder->event_at=$request->json('event_at');
-                $reminder->save();        
-                return $this->sendResponse(new ReminderResource($reminder), 0,201);
-            }
-            
+        } catch (\Illuminate\Database\QueryException $exception) {
+            return $this->sendError(ERR_INTERNAL_ERROR, "unable to connect into database", 500);
         }
     }
 
@@ -179,26 +199,30 @@ class ReminderController extends BaseController
     {
         if (!empty($request->header('Authorization'))) {
             $parts = explode(' ', $request->header('Authorization'));
-    
+
             if (count($parts) !== 2 || strtolower($parts[0]) !== 'bearer') {
-                return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+                return $this->sendError(ERR_INVALID_ACCESS_TOKEN, "invalid access token", 401);
             }
             $refreshToken = $parts[1];
         } else {
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
+            return $this->sendError(ERR_INVALID_ACCESS_TOKEN, "invalid access token", 401);
         }
-        $token = PersonalAccessToken::findToken($refreshToken);
-        if(empty($token)){
-            return $this->sendError(ERR_INVALID_REFRESH_TOKEN,"invalid refresh token",401); 
-        } else {
-                        
-            $reminder = Reminder::find($id);
-            if($reminder==null){
-                return $this->sendError(ERR_NOT_FOUND, 'data has not found',404);  
-            }else{
-                $reminder->delete();
-                return $this->sendResponse(new ReminderResource($reminder), 0,201);
+        try {
+            $token = PersonalAccessToken::findToken($refreshToken);
+            if (empty($token)) {
+                return $this->sendError(ERR_FORBIDDEN_ACCESS, "user doesn't have enough authorization", 403);
+            } else {
+
+                $reminder = Reminder::find($id);
+                if ($reminder == null) {
+                    return $this->sendError(ERR_NOT_FOUND, 'resource is not found', 404);
+                } else {
+                    $reminder->delete();
+                    return $this->sendResponse(new ReminderResource($reminder), 0, 201);
+                }
             }
+        } catch (\Illuminate\Database\QueryException $exception) {
+            return $this->sendError(ERR_INTERNAL_ERROR, "unable to connect into database", 500);
         }
     }
 }
